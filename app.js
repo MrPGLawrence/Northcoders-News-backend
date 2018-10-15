49882cae9f685cbf420368f939dc1c47627e5dc3
Patrick Lawrence <lwrncp@yahoo.com>
2018-10-15 17:26:41 +0100

    changed app.js

--- app.js
+++ app.js
@@ -20,10 +20,6 @@ app.get("/products/:id", function(req, res, next) {
   res.json({ msg: "This is CORS-enabled for all origins!" });
 });
 
-app.listen(80, function() {
-  console.log("CORS-enabled web server listening on port 80");
-});
-
 mongoose
   .connect(
     DB_URL,
